53ebe19cd7fb049aa112fe5eadbb5b644526392f
Namazu <namazutitech15@gmail.com>
2018-07-14 20:53:45 +0900

    make fbul recursive

--- bullet.js
+++ bullet.js
@@ -30,6 +30,7 @@ class FBullet {
     this.y = y;
     this.v = v;
     this.bul = [];
+    this.fbul = [];
     this.theta = theta;
   }
   update(){
@@ -40,6 +41,9 @@ class FBullet {
     this.bul.forEach((v)=>{
       v.draw(ctx,this.x,this.y,this.theta);
     })
+    this.fbul.forEach(v)=>{
+      v.draw(ctx)
+    }
   }
 }
 
